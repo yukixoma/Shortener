0553d02f5e89ce4399a31102ae4740dc5bf4d1d8
yukixoma <yukixoma08@gmail.com>
2017-12-05 21:07:56 +0700

    make it better

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get(path.join(__dirname,"1"),function(req,res){
     res.redirect(originalLink);
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Working");
 })
