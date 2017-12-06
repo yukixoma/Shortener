b2067e379ac0c5ce3f9653da719252d43d0c2e8e
yukixoma <yukixoma08@gmail.com>
2017-12-06 12:11:02 +0700

    update

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var app = module.exports = express();
+var link;
+var holder = [];
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + "/public"))
 
-var link;
 
 app.get("/o/:link(*)",function(req,res){
     if (req.params.link == "favicon.ico") {
@@ -17,9 +19,12 @@ app.get("/o/:link(*)",function(req,res){
     } else {
         link = req.params.link;
     }     
-    console.log(link);
+    console.log(link); 
+    var holderIndex = Math.floor(Math.random()*1000).toString();
+    holder[holderIndex] = link;   
     var originalLink = req.protocol + "s://" + req.get("host") + "/" + link;
-    var shortenLink  = req.protocol + "s://" + req.get("host") + "/s";
+    var shortenLink  = req.protocol + "s://" + req.get("host") + "/s/" + holderIndex;
+    
 
     res.json({
         "original": originalLink.toString(),
@@ -27,10 +32,14 @@ app.get("/o/:link(*)",function(req,res){
     });
 })
 
-
-app.get("/s",function(req,res,next){
-    console.log("redirect working" + link);
-    res.redirect( link );    
+app.get("/s/:holderIndex",function(req,res,next){
+    var holderIndex = req.params.holderIndex;
+    link = holder[holderIndex];
+    console.log("redirect working" +link);
+    if(link[0]+link[1]+link[2]+link[3] != "http") {
+        link = "https://" + link;
+    }
+    res.redirect(link);    
 })
 
 app.listen(process.env.PORT || 3000, function(){
